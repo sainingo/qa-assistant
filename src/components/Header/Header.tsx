1742510582194b678ed3b5216c398e8f8c89d2de
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2023-03-16 10:13:14 +0300

    Dap 7 nav bars (#23)
    * Update README.md
    
    * DAP 7 - Add navbar and sidebar
    
    ---------
    
    Co-authored-by: Kipchumba C. Bett <kbett68@gmail.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import logo from '../../../public/ampath-logo.jpeg'
+import { Link } from 'react-router-dom'
+import {MdLogout} from 'react-icons/md'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { useNavigate } from 'react-router-dom'
+
+const Header = () => {
+  const navigate = useNavigate()
+  const handleLogout = async () => {
+    try {
+      await fetch('openmrs/ws/rest/v1/session', {
+        method: 'DELETE',
+        })
+        .then(() => {
+          localStorage.removeItem('userInformation')
+          localStorage.removeItem('authenticated')
+          navigate('/login')
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div className='h-28'>
+         <header className='bg-white shadow-md flex justify-between items-center'>
+            <span className='cursor-pointer ml-3'>
+                <Link to="/"><img src={logo} width={180} /></Link>
+            </span>
+            <button className='text-3xl mr-4 md:hidden'><GiHamburgerMenu /></button>
+            <nav className='hidden md:block md:mr-12'>
+                <ul className='md:flex items-center gap-20'>
+                    <li className='text-sm'>Logged in as <span className='uppercase'><strong>ADMIN</strong></span></li>
+                    <li onClick={handleLogout} className='text-lg flex gap-2 items-center hover:shadow-lg hover:bg-slate-300 p-2 rounded-sm cursor-pointer'><MdLogout />Log out</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+export default Header
---
+++ src/components/SideNavBar/SideNavBar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {AiOutlineInfoCircle} from 'react-icons/ai'
+import {BsPencilSquare} from 'react-icons/bs'
+import {FaWalking, FaRegCalendarAlt} from 'react-icons/fa'
+import {BiBandAid} from 'react-icons/bi'
+import { Link, NavLink } from 'react-router-dom'
+const SideNavBar = () => {
+  let activeStyle = {
+    color: '#2FBAF1',
+    cursor: 'pointer'
+    // hover:cursor-pointer hover:shadow-md text-blue-300
+  };
+  return (
+    <div className='hidden md:block absolute bg-white h-screen pl-[2%] shadow-lg'>
+        <nav className='mt-20'>
+            <ul className='flex flex-col gap-10 text-xl mr-2'>
+                <NavLink to='/patients/:id' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                <li className='py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md'><AiOutlineInfoCircle /> Patients Info</li>
+                </NavLink>
+                <NavLink to="/orders" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                <li className='py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md'><BsPencilSquare />Orders</li>
+                </NavLink>
+                <NavLink to="/visits" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                <li className='py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md'><FaWalking />Visits</li>
+                </NavLink>
+                <NavLink to="/encounters" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                <li className='py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md'><BiBandAid />Encounters</li>
+                </NavLink>
+                <NavLink to="/observations" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                <li className='py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md'><FaRegCalendarAlt />Observations</li>
+                </NavLink>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+export default SideNavBar
