12bd5db3b232332ff2f3d37580207e7efa8c70c0
sainingo <nickmentor421@gmail.com>
2023-03-22 15:49:02 +0300

    Comment out vite's config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    // proxy: {
-    //   "/openmrs": {
-    //     target: "https://dev3.openmrs.org",
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
+    proxy: {
+      "/openmrs": {
+        target: "https://dev3.openmrs.org",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-  test: {
-    globals:true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.ts',
-  },
+  // test: {
+  //   globals:true,
+  //   environment: 'jsdom',
+  //   setupFiles: './tests/setup.ts',
+  // },
 });
