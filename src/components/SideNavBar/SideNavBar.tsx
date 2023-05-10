70301fa708034db8a14507a4701ab5b7143f53b1
Saningo Lekalantula <nickmentor421@gmail.com>
2023-05-10 10:31:53 +0300

    Observations (#51)
    Co-authored-by: Kipchumba Bett <kbett68@gmail.com>

--- src/components/SideNavBar/SideNavBar.tsx
+++ src/components/SideNavBar/SideNavBar.tsx
@@ -6,6 +6,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { BiBandAid } from 'react-icons/bi';
 import { NavLink } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
+
 const SideNavBar = () => {
   const [open, setOpen] = useState(true);
 
@@ -25,45 +26,43 @@ const SideNavBar = () => {
   };
 
   return (
-    <div>
-      <div className={`${open ? 'absolute bg-white h-screen pl-[2%] shadow-lg' : 'absolute bg-white h-screen -ml-28'}`}>
-        <button className="text-3xl ml-32" onClick={handleToggleMenu}>
-          <GiHamburgerMenu />
-        </button>
-        <nav className={`${open ? 'mt-20' : 'opacity-5 -ml-6'}`}>
-          <ul className="flex flex-col gap-10 text-xl mr-2">
-            <NavLink to={`/patientInfo/${patientId}`} style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-              <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
-                <AiOutlineInfoCircle /> Patients Info
-              </li>
-            </NavLink>
-            <NavLink to={`/patient/${patientId}/orders`} style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-              <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
-                <BsPencilSquare />
-                Orders
-              </li>
-            </NavLink>
-            <NavLink to="/visits" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-              <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
-                <FaWalking />
-                Visits
-              </li>
-            </NavLink>
-            <NavLink to="/encounters" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-              <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
-                <BiBandAid />
-                Encounters
-              </li>
-            </NavLink>
-            <NavLink to="/observations" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-              <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
-                <FaRegCalendarAlt />
-                Observations
-              </li>
-            </NavLink>
-          </ul>
-        </nav>
-      </div>
+    <div className="hidden md:block absolute bg-white h-screen pl-[2%] shadow-lg">
+      <nav className="mt-20">
+        <ul className="flex flex-col gap-10 text-xl mr-2">
+          <NavLink to={`/patientInfo/${patientId}`} style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
+              <span ><AiOutlineInfoCircle /> </span>Patients Info
+            </li>
+          </NavLink>
+          <NavLink to={`/patient/${patientId}/orders`} style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
+              <BsPencilSquare />
+              Orders
+            </li>
+          </NavLink>
+          <NavLink to="/visits" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
+              <FaWalking />
+              Visits
+            </li>
+          </NavLink>
+          <NavLink to="/encounters" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+            <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
+              <BiBandAid />
+              Encounters
+            </li>
+          </NavLink>
+          <NavLink
+            to={`/observations/${patientId}`}
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            <li className="py-2 px-4 flex gap-2 items-center hover:cursor-pointer hover:shadow-md">
+              <FaRegCalendarAlt />
+              Observations
+            </li>
+          </NavLink>
+        </ul>
+      </nav>
     </div>
   );
 };
